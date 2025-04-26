0f768bd66f3da50d51224eb374937ae0d2fe4b87
awaisrajput99 <162193480+awaisrajput99@users.noreply.github.com>
2025-04-27 00:15:55 +0500

    appdelegate.swift file changed to default

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,21 +10,6 @@ import GoogleMaps
   ) -> Bool {
     GMSServices.provideAPIKey("AIzaSyC8JtIMqTQ8KByebN3hnijwXPZnt3wzrRs")
     GeneratedPluginRegistrant.register(with: self)
-
-     // Set UNUserNotificationCenter delegate
-        if #available(iOS 10.0, *) {
-          UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-        }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    // Handle notification taps when app is killed
-    override func userNotificationCenter(
-      _ center: UNUserNotificationCenter,
-      didReceive response: UNNotificationResponse,
-      withCompletionHandler completionHandler: @escaping () -> Void
-    ) {
-      let userInfo = response.notification.request.content.userInfo
-      Messaging.messaging().appDidReceiveMessage(userInfo)
-      completionHandler()
-    }
 }
